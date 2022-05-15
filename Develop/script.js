827e019fc89e8af76a177e69cb16054830484c3c
unknown <mcsalazar122@gmail.com>
2022-05-15 11:16:40 -0500

    prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+//initial question
+window.prompt("How many characters would you like in your password?");
 
+function pwlength (){
+  
+}
+pwlength()
+//based on answer return a specific number
+//var pwlength ()
+
+//case questions
+window.prompt("Do you need Upper Case letters?");
+//upperCase()
+
+window.prompt("Do you need lower case letters?");
+//lowerCase()
+//Numbers question
+
+window.prompt("Inlcude a number?");
+//Number()
+//Special characters
+window.prompt("Any special characters ex. #$*@?");
+//Specials()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+# Password Generator 
+
+## My password generator is created to generate a random password that meets the user requirements.  To accomplish this, I need to add the following criteria:
+
+-Button will prompt a questions to the user
+-Upon answer, prompt for additional clarifying questions, upon answer, must include one of each true answer:
+    -how many characters
+    -uppercase
+    -lowercase
+    -special characters
+    -numbers
+-Return random password meeting these criteria
+-Ask to start again?
+-Do we want to store in local setting?
+-Do we want to be able to recall this data?
